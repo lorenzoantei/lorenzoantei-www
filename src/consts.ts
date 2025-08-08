909cea100e21703d39d808f365477b9372f0ddbd
Lorenzo Antei <lorenzoantei@protonmail.com>
2025-08-08 12:46:44 +0200

    rebuilt all from blog boilerplate

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Lorenzo Antei';
-export const SITE_DESCRIPTION = 'Hi!';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
