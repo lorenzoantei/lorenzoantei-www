0c8dadb0c22a6c1c9edaad736063f7b6b80b1179
Lorenzo Antei <lorenzoantei@protonmail.com>
2025-09-18 15:14:01 +0200

    added translate articles

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,25 @@
+// src/content/config.ts
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
-			published: z.boolean().default(false),
-			tags: z.array(z.string()).default([]),
-		}),
-});
+const baseSchema = ({ image }: any) =>
+  z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: image().optional(),
+    published: z.boolean().default(false),
+    tags: z.array(z.string()).default([]),
+  });
 
-export const collections = { blog };
+const makeCollection = (lang: string) =>
+  defineCollection({
+    loader: glob({ base: `./src/exps/${lang}`, pattern: '**/*.{md,mdx}' }),
+    schema: baseSchema,
+  });
+
+export const collections = {
+  expsEnList: makeCollection('en'),
+  expsItList: makeCollection('it'),
+};
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const exps = await getCollection('expsItList');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
+		items: exps.map((exp) => ({
+			...exp.data,
+			link: `/blog/${exp.id}/`,
 		})),
 	});
 }
